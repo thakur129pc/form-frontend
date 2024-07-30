f9b84c4b9b8ec1116eab70dde371b1a2da0f7ec6
thakur129pc <thakur129pc@gmail.com>
2024-07-30 17:23:58 +0530

    last Commit

--- src/pages/register/components/ThirdStep.jsx
+++ src/pages/register/components/ThirdStep.jsx
@@ -38,10 +38,9 @@ const ThirdStep = ({ data }) => {
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
         "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character"
       ),
-    confirmPassword: Yup.string().oneOf(
-      [Yup.ref("password"), null],
-      "Passwords must match"
-    ),
+    confirmPassword: Yup.string()
+      .required("Confirm Password is required")
+      .oneOf([Yup.ref("password"), null], "Passwords must match"),
   });
 
   const previousStep = (values) => {
