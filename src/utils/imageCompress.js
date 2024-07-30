3fee84bd8328e6738ae07f7e97bbad3928c8fa58
thakur129pc <thakur129pc@gmail.com>
2024-07-30 10:13:27 +0530

    image set

---
+++ src/utils/imageCompress.js
@@ -0,0 +1,16 @@
+import ImageCompression from "browser-image-compression";
+
+export const compressImage = async (file) => {
+  const options = {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+  };
+
+  try {
+    const compressedFile = await ImageCompression(file, options);
+    return compressedFile;
+  } catch (error) {
+    console.error(error);
+  }
+};
